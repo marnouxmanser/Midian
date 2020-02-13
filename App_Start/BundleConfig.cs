3978fbff894eb482755744e2ae7f42dda2df4e88
Marnoux Manser <marnouxmanser@gmail.com>
2020-02-13 11:00:04 +0200

    Optimize layout

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Midian
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-dark.css",
                       "~/Content/site.css"));
         }
     }
